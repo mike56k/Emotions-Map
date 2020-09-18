660e3f4dbcb1e2b359dd47de8eaa127129cf78f1
mike56k <miha.is@mail.ru>
2020-09-18 23:24:02 +0300

    lol

--- src/panels/Map.js
+++ src/panels/Map.js
@@ -7,13 +7,13 @@ import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton
 import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
 import Icon24Back from "@vkontakte/icons/dist/24/back";
 import SearchCard from "./SearchCard";
-import { MapContainer } from "./MapContainer";
+import SimpleMap from "./MapContainer";
 
 const osName = platform();
 
 const Map = (props) => (
   <Panel id={props.id}>
-    <MapContainer />
+    <SimpleMap />
   </Panel>
 );
 
--- src/panels/MapContainer.js
+++ src/panels/MapContainer.js
@@ -1,21 +1,50 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import React, { Component } from "react";
-export class MapContainer extends Component {
+import GoogleMapReact from "google-map-react";
+import MyGreatPlace from "./MyGreatPlace.js";
+import PropTypes from "prop-types";
+import SearchCard from "./SearchCard.js";
+import "./mapcontainer.css";
+class SimpleMap extends Component {
+  static propTypes = {
+    center: PropTypes.array,
+    zoom: PropTypes.number,
+    greatPlaceCoords: PropTypes.any,
+  };
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  };
+
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
-
-        {/* <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div> 
-        </InfoWindow> */}
-      </Map>
+      // Important! Always set the container height explicitly
+      <div>
+        <div style={{ height: "100vh", width: "100%" }} id="gmap">
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyBKw11akhPLco6hZSw8IU3heygOKz7150M",
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <MyGreatPlace
+              lat={59.955413}
+              lng={30.337844}
+              text={"🛵\nСкутер"} /* Kreyser Avrora */
+            />
+            <MyGreatPlace
+              {...this.props.greatPlaceCoords}
+              text={"B"} /* road circle */
+            />
+          </GoogleMapReact>
+        </div>
+        <SearchCard class="container overlap" />{" "}
+      </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "YOUR_GOOGLE_API_KEY_GOES_HERE",
-})(MapContainer);
+export default SimpleMap;
