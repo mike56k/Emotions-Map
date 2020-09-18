b609dc3d16ad57aee6e097542c6dafa6629b08c3
mike56k <miha.is@mail.ru>
2020-09-18 21:43:55 +0300

    Updates

---
+++ precache-manifest.331051f37c7b68c3bde0774b5bd0d249.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29438dbb9ce626700dca3af1e22c54d",
+    "url": "/Emotions-Map/index.html"
+  },
+  {
+    "revision": "a5944612b56c697821ed",
+    "url": "/Emotions-Map/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "9b9a71346b1fda13c2b2",
+    "url": "/Emotions-Map/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a5944612b56c697821ed",
+    "url": "/Emotions-Map/static/js/2.34350524.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/Emotions-Map/static/js/2.34350524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9a71346b1fda13c2b2",
+    "url": "/Emotions-Map/static/js/main.02c7d9a3.chunk.js"
+  },
+  {
+    "revision": "4a8f30911ca66f216f9f",
+    "url": "/Emotions-Map/static/js/runtime-main.73195563.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Emotions-Map/static/media/persik.4e1ec840.png"
+  }
+]);
