6946c3c33e8483e09f61e5b6a0a97ed645e2d5fb
mike56k <miha.is@mail.ru>
2020-09-18 23:43:11 +0300

    bridge

--- src/panels/components/Post.js
+++ src/panels/components/Post.js
@@ -1,46 +1,55 @@
 import React, { useEffect, useState } from "react";
-import {CardGrid, Card, RichCell, Avatar, View, Panel, PanelHeader, FormLayout, Checkbox, Link, Div, Button, Group, Header} from "@vkontakte/vkui";
+import {
+  CardGrid,
+  Card,
+  RichCell,
+  Avatar,
+  View,
+  Panel,
+  PanelHeader,
+  FormLayout,
+  Checkbox,
+  Link,
+  Div,
+  Button,
+  Group,
+  Header,
+} from "@vkontakte/vkui";
 import Icon24LikeOutline from "@vkontakte/icons/dist/24/like_outline";
 import Icon24ShareOutline from "@vkontakte/icons/dist/24/share_outline";
 import Icon16MoreHorizontal from "@vkontakte/icons/dist/16/more_horizontal";
-import bridge from '@vkontakte/vk-bridge';
+import bridge from "@vkontakte/vk-bridge";
 
-const Post =()=> {
-    const [user, setUser]=useState(null);
-    useEffect(()=> {
-		bridge.send('VKWebAppGetUserInfo', {})
-			.then((user) => {
-				setUser({ user });
-			});
-	});
-    return (<CardGrid>
-        <Card size="l" mode="shadow" style={{height:324}}>
-            {
-                 user ?
-                 <RichCell
-                 before={<Avatar size={48} />}
-                 >{user.first_name}
-                 </RichCell>
-                   
-                   :
-                   'VK Apps Image Example'
-            }
-            <div style={{height:162, background:"red"}}></div>
-           
-         
-         
-          <Div style={{display: 'flex' }}>
+const Post = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    bridge.send("VKWebAppGetUserInfo", {}).then((user) => {
+      setUser({ user });
+    });
+  });
+  return (
+    <CardGrid>
+      <Card size="l" mode="shadow" style={{ height: 324 }}>
+        {user ? (
+          <RichCell before={<Avatar size={48} />}>{user.first_name}</RichCell>
+        ) : (
+          "VK Apps Image Example"
+        )}
+        <div style={{ height: 162, background: "red" }}></div>
+
+        <Div style={{ display: "flex" }}>
           <Group header={<Header mode="secondary">15 комментариев</Header>}>
-       <Button   style={{ marginRight: 8,  backgroundColor:"transparent" }}><Icon24LikeOutline style={{color: "#99A2AD"}} /></Button>
-       <Button   style={{ marginRight: 8, backgroundColor:"transparent" }}><Icon24ShareOutline style={{color: "#99A2AD"}} /></Button>
-       </Group>
-     </Div>    
-        </Card>
-        
-        
-       
-      </CardGrid>
-      );
-  }
+            <Button style={{ marginRight: 8, backgroundColor: "transparent" }}>
+              <Icon24LikeOutline style={{ color: "#99A2AD" }} />
+            </Button>
+            <Button style={{ marginRight: 8, backgroundColor: "transparent" }}>
+              <Icon24ShareOutline style={{ color: "#99A2AD" }} />
+            </Button>
+          </Group>
+        </Div>
+      </Card>
+    </CardGrid>
+  );
+};
 
-  export default Post;
+export default Post;
